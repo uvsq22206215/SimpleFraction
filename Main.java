243be829fa4765f8d06d0696a5884a2b25d9fe02
Tasneem Durbass <rivieretasneem@hotmail.com>
2022-09-30 14:52:43 +0200

    added assert in main method

--- Main.java
+++ Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Fraction maFraction = new Fraction();
         System.out.println(maFraction);
+
+        Fraction FT_1 = new Fraction(12, 9);
+
+        // assert FT_1.getNumerateur() == 0 : "Le valeur du numérateur n'est pas 0";
+        assert FT_1.toString() == "Je suis";
+        assert args.length > 0;
     }
 }
