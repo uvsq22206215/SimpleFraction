79752e7f2243e30d38fe45d0f2462b1387fc6710
Tasneem Durbass <rivieretasneem@hotmail.com>
2022-09-30 14:23:10 +0200

    added constructor and attributes to Fraction class + Gitignore file

--- Fraction.java
+++ Fraction.java
@@ -1,6 +1,40 @@
 public class Fraction {
 
+    private int numerateur;
+    private int denominateur;
+
+    Fraction(int num_, int denom_){
+        this.numerateur = num_;
+        this.denominateur = denom_;
+    }
+
+    Fraction(int num_){
+        this.numerateur = num_;
+        this.denominateur = 1;
+    }
+
+    Fraction(){
+        this.numerateur = 0;
+        this.denominateur = 1;
+    }
+
     public String toString(){//overriding the toString() method
         return "Je suis une fraction.";
+    }
+
+    public int getNumerateur(){
+        return this.numerateur;
+    }
+
+    public void setNumerateur(int newNum){
+        this.numerateur = newNum;
+    }
+
+    public int getDenominateur(){
+        return this.denominateur;
+    }
+
+    public void setDenominateur(int newDenom){
+        this.denominateur = newDenom;
     }
 }
