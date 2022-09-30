8e2595d2efe456f33705622ea3da44f65011fab6
Tasneem Durbass <rivieretasneem@hotmail.com>
2022-09-30 14:56:53 +0200

    added constant Fraction objects

--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
 public class Main {
+
+    public static final Fraction fraction_zero = new Fraction(0,1);
+    public static final Fraction fraction_un = new Fraction(1,1);
+
     public static void main(String[] args) {
         Fraction maFraction = new Fraction();
         System.out.println(maFraction);
 
         Fraction FT_1 = new Fraction(12, 9);
 
-        // assert FT_1.getNumerateur() == 0 : "Le valeur du numérateur n'est pas 0";
-        assert FT_1.toString() == "Je suis";
-        assert args.length > 0;
+        assert FT_1.getNumerateur() == 0 : "Le valeur du numérateur n'est pas 0";
+        // assert FT_1.toString() == "Je suis";
+
+
     }
 }
